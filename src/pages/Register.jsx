5cbbdac521ae02a13b86c03b8f87c63186a09e0f
mito-coder <abhishek.prajapati.it@gmail.com>
2026-04-17 12:22:26 +0530

    AuthContext added

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { useSearchParams } from "react-router-dom";
+import { useAuth } from "../context/AuthContext";
 
 function Register() {
   const [searchparam] = useSearchParams();
