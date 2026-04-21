fbbf32d02bf8318510160aca9de4104367ffabd4
mito-coder <abhishek.prajapati.it@gmail.com>
2026-04-21 22:03:36 +0530

    Register, Login API complete

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -31,26 +31,35 @@ const register = async (req, res) => {
 };
 
 const login = async (req, res) => {
-  const { email, password } = req.body;
-  const user = await User.findOne({ email });
-  if (!user) {
-    return res.status(400).json({ message: "User not found " });
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ message: "User not found " });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+      return res.status(400).json({ message: "Wrong password" });
+    }
+    const token = jwt.sign(
+      { id: user._id, role: user.role },
+      process.env.JWT_SECRET,
+      {
+        expiresIn: "7d",
+      },
+    );
+
+    res.status(200).json({
+      message: "Login successful!",
+      token,
+      user,
+    });
+  } catch (error) {
+    res.status(500).json({ message: "Server error!", error: error.message });
   }
-
-  const isMatch = await bcrypt.compare(password, user.password);
-
-  if (!isMatch) {
-    return res.status(400).json({ message: "Wrong password" });
-  }
-  const token = jwt.sign({ id: user._id, role: user.role }, "mysecretkey", {
-    expiresIn: "7d",
-  });
-
-  res.status(200).json({
-    message: "Login successful!",
-    token,
-    user,
-  });
 };
 
 module.exports = { register, login };
