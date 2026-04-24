2bf00d9a4b2b54d5388163bbf60fdf2319939c86
mito-coder <abhishek.prajapati.it@gmail.com>
2026-04-24 21:21:28 +0530

    feat: add postedBy field in Cargo schema

---
+++ server/middleware/authMiddleware.js
@@ -0,0 +1,21 @@
+const jwt = require("jsonwebtoken");
+
+const protect = (req, res, next) => {
+  try {
+    // Extract token from the header.
+    const token = req.headers.authorization?.split(" ")[1];
+
+    if (!token) {
+      return res.status(401).json({ message: "No token, access denied!" });
+    }
+
+    // Token verify
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded; // { id, role } It is now available in the req.
+    next();
+  } catch (error) {
+    res.status(401).json({ message: "Invalid token!" });
+  }
+};
+
+module.exports = { protect };
