9aa837217becebdbe75ea247931d9d4b88456ae7
mito-coder <abhishek.prajapati.it@gmail.com>
2026-04-25 11:56:05 +0530

    update

--- server/routes/cargoRoutes.js
+++ server/routes/cargoRoutes.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const { postCargo } = require("../controllers/cargoController");
+const { postCargo, getMyCargos } = require("../controllers/cargoController");
+const { Protect } = require("../middleware/authMiddleware");
 
 router.post("/post", postCargo);
+router.get("/my-cargos", Protect, getMyCargos);
 
 module.exports = router;
