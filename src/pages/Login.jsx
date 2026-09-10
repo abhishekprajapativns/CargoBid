a0f5960bfd110fdb5dfd5e7107240d622d4bfe09
Abhishek Prajapati <abhishek.prajapati.it@gmail.com>
2026-09-10 11:44:49 +0530

    fix: replace class with className in Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -71,9 +71,9 @@ function Login() {
           Login
         </button>
 
-        <p class="text-center mt-4 text-sm">
+        <p className="text-center mt-4 text-sm">
           Don’t have an account?{" "}
-          <a class="text-blue-600" href="/register" data-discover="true">
+          <a className="text-blue-600" href="/register" data-discover="true">
             Register
           </a>
         </p>
